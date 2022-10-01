c0c2e5bffebccffad9f416873dd052dabd715681
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-01 16:12:12 +0530

    [Added: checking For Full Time Salary]

--- src/com/blabz/java/Emp_Wage_Problem.java
+++ src/com/blabz/java/Emp_Wage_Problem.java
@@ -6,13 +6,17 @@ public class Emp_Wage_Problem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random random = new Random();
+		
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int salary = 0;
 		int randomNum = random.nextInt(2);
-
-		if (randomNum == 1)
+		if (randomNum == 1) {
 			System.out.println("Employee  is Present");
-		else
+			salary = wagePerHour * fullDayHours;
+		} else
 			System.out.println("Employee  is Absent");
-
+		System.out.println("salary is: "+salary);
 	}
 
 
