81b5e3d93c5a0dd434e20ad01d3adf84981af8b0
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-01 16:18:41 +0530

    [Added: Getting Salary Using Switch case]

--- src/com/blabz/java/Emp_Wage_Problem.java
+++ src/com/blabz/java/Emp_Wage_Problem.java
@@ -6,6 +6,7 @@ public class Emp_Wage_Problem {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
 		Random random = new Random();
 
 		int wagePerHour = 20;
@@ -13,17 +14,26 @@ public class Emp_Wage_Problem {
 		int halfDayHours = 4;
 		int salary = 0;
 		int randomNum = random.nextInt(3);
-		if (randomNum == 1) {
-			System.out.println("Employee  is Present");
+		
+		switch(randomNum) {
+		
+		case 0:
+			System.out.println("Employee is absent");
+			System.out.println("Monthly Salary wil be:~ ");
+			break;
+		case 1:
+			System.out.println("Employee is Present");
+			System.out.println("Monthly salary for Full Day :~ ");
 			salary = wagePerHour * fullDayHours;
-			System.out.println("Monthly salary for Full day is: ");
-		} else if (randomNum == 2) {
-			salary = halfDayHours * wagePerHour;
-
-			System.out.println("Monthly salary for Half day is: ");
-		} else
-			System.out.println("Employee  is Absent");
-		System.out.println("salary is: " + salary);
+			break;
+		case 2:
+			System.out.println("Employee is present");
+			System.out.println("Monthly Salary for Half Day:~ ");
+			salary = wagePerHour * halfDayHours;
+			break;
+		}
+			System.out.println(+salary);
+			
 	}
 
 }
