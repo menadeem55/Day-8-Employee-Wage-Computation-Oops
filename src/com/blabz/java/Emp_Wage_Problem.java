6eb3864ed75d33abe0249bc1e69de0b49841ba28
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-01 16:15:44 +0530

    [Added: Part Time Salary]

--- src/com/blabz/java/Emp_Wage_Problem.java
+++ src/com/blabz/java/Emp_Wage_Problem.java
@@ -4,20 +4,26 @@ import java.util.*;
 
 public class Emp_Wage_Problem {
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random random = new Random();
-		
+
 		int wagePerHour = 20;
 		int fullDayHours = 8;
+		int halfDayHours = 4;
 		int salary = 0;
-		int randomNum = random.nextInt(2);
+		int randomNum = random.nextInt(3);
 		if (randomNum == 1) {
 			System.out.println("Employee  is Present");
 			salary = wagePerHour * fullDayHours;
+			System.out.println("Monthly salary for Full day is: ");
+		} else if (randomNum == 2) {
+			salary = halfDayHours * wagePerHour;
+
+			System.out.println("Monthly salary for Half day is: ");
 		} else
 			System.out.println("Employee  is Absent");
-		System.out.println("salary is: "+salary);
+		System.out.println("salary is: " + salary);
 	}
-
 
 }
