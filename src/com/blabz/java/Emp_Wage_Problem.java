74d155e71705befa3cc2a7a8847d3c68cb9f241e
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-01 16:30:36 +0530

    [Added: Getting Monthly Salary With Condition]

--- src/com/blabz/java/Emp_Wage_Problem.java
+++ src/com/blabz/java/Emp_Wage_Problem.java
@@ -3,6 +3,7 @@ package com.blabz.java;
 import java.util.*;
 
 public class Emp_Wage_Problem {
+
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
@@ -16,33 +17,35 @@ public class Emp_Wage_Problem {
 		int salary = 0;
 		int monthlysalary = 0;
 		int workingDaysPerMonth = 20;
-		
-		for(int i = 1; i <= workingDaysPerMonth; i++) {
-		int randomNum = random.nextInt(3);
-		
-		switch(randomNum) {
-		
-		case 0:
-			System.out.println("Employee is absent");
-			System.out.println("Salary wil be:~ ");
-			break;
-		case 1:
-			System.out.println("Employee is Present");
-			System.out.println("salary for Full Day :~ ");
-			salary = wagePerHour * fullDayHours;
-			break;
-		case 2:
-			System.out.println("Employee is present");
-			System.out.println("Salary for Half Day:~ ");
-			salary = wagePerHour * halfDayHours;
-			break;
-		}
+		int days = 0;
+		int monthlyHours = 0;
+
+		while (days != 20 && monthlyHours != 100) {
+			days++;
+			int randomNum = random.nextInt(3);
+
+			switch (randomNum) {
+
+			case 0:
+				// System.out.println("Employee is absent");
+				// System.out.println("Salary wil be:~ ");
+				break;
+			case 1:
+				// System.out.println("Employee is Present For Full Day");
+				System.out.println("salary for Full Day :~ ");
+				salary = wagePerHour * fullDayHours;
+				break;
+			case 2:
+				// System.out.println("Employee is present for Half Day");
+				System.out.println("Salary for Half Day:~ ");
+				salary = wagePerHour * halfDayHours;
+
+			}
 			monthlysalary = monthlysalary + salary;
-			System.out.println(+salary);
-			
+			System.out.println("Day " + days + " Salary is:" + salary);
+
 		}
-		System.out.println("Monthly Salary:~"+monthlysalary);
-			
+		System.out.println("Monthly Salary:~" + monthlysalary);
 	}
 
 }
