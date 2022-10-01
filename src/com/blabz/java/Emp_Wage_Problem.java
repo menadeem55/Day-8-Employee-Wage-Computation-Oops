76540a45787d5a7c1d378ac6f91d34767cbfc0c9
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-01 16:07:03 +0530

    [Added: Employee Attendence]

--- src/com/blabz/java/Emp_Wage_Problem.java
+++ src/com/blabz/java/Emp_Wage_Problem.java
@@ -1,8 +1,19 @@
 package com.blabz.java;
 
+import java.util.*;
+
 public class Emp_Wage_Problem {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+
+		if (randomNum == 1)
+			System.out.println("Employee  is Present");
+		else
+			System.out.println("Employee  is Absent");
+
 	}
+
 
 }
